e2f000dd989f07f6442aef667bb0f26091c183f1
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2021-06-13 17:54:32 +0530

    Publishing the App to Github Pages

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox placeholder="Search here" handleChange={this.handleChange} />
         <CardList monsters={filteredMonsters}></CardList>
       </div>
